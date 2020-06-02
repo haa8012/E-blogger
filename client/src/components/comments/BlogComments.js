332e65abcb45cf13e1cb6f1704562b1d717a521a
Hani A <ha8012@yahoo.com>
2020-06-01 21:40:05 -0400

    added comments  button

--- client/src/components/comments/BlogComments.js
+++ client/src/components/comments/BlogComments.js
@@ -1,10 +1,21 @@
 import React, { useContext, useEffect, Fragment } from 'react';
 import CommentContext from '../../context/comment/commentContext';
+import UserContext from '../../context/auth/authContext';
 import CommentForm from './CommentForm';
 
 function BlogComments({ id }) {
   const commentContext = useContext(CommentContext);
-  const { comments, getComments, loading } = commentContext;
+  const userContext = useContext(UserContext);
+
+  const { user } = userContext;
+
+  const {
+    comments,
+    getComments,
+    loading,
+    setCurrentComment,
+    deleteComment,
+  } = commentContext;
 
   useEffect(() => {
     getComments(id);
@@ -13,13 +24,45 @@ function BlogComments({ id }) {
 
   const blogComments = comments;
 
+  const onDelete = (id) => {
+    deleteComment(id);
+    // clearCurrent();
+  };
+
+  const onEdit = (comment) => {
+    setCurrentComment(comment);
+  };
+
+  if (user !== null) {
+    console.log('user logged in ');
+  } else {
+    console.log('No user logged in ');
+  }
+
   return (
     <Fragment>
-      <CommentForm />
+      <CommentForm id={id} />
       <div className='comment-container'>
         {blogComments != null ? (
           blogComments.map((comment) => (
-            <div className='comment'>{comment.content}</div>
+            <div>
+              <div className='comment' key={comment._id}>
+                {comment.content}
+              </div>
+              {user && (
+                <p className='p-2 text-right'>
+                  {/* <button
+                    className='btn btn-dark btn-sm '
+                    onClick={onEdit(comment)}
+                  >
+                    Edit
+                  </button>
+                  <button className='btn btn-danger btn-sm ' onClick={onDelete}>
+                    Delete
+                  </button> */}
+                </p>
+              )}
+            </div>
           ))
         ) : (
           <div></div>
--- client/src/components/comments/CommentForm.js
+++ client/src/components/comments/CommentForm.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import CommentContext from '../../context/comment/commentContext';
 
-function CommentForm() {
-  const addComment = () => {};
+function CommentForm({ id }) {
+  const commentContext = useContext(CommentContext);
+  const {
+    addComment,
+    currentComment,
+    updateComment,
+    setCurrentComment,
+  } = commentContext;
+
+  useEffect(() => {
+    if (currentComment !== null) {
+      setCmnt(currentComment);
+    } else {
+      setCmnt({
+        id: id,
+        content: '',
+      });
+    }
+  }, [currentComment]);
+
+  const [cmnt, setCmnt] = useState({ id: id, content: '' });
+
+  const onChange = (e) => {
+    setCmnt({ ...cmnt, [e.target.name]: e.target.value });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (currentComment === null) {
+      addComment(cmnt);
+    } else {
+      updateComment(cmnt);
+    }
+  };
+
+  const { content } = cmnt;
+
   return (
     <div
       style={{
@@ -12,8 +48,8 @@ function CommentForm() {
         border: '1px dashed #c7c7c7',
       }}
     >
-      <input type='text' />
-      <button className='btn btn-dark btn-block' onClick={addComment}>
+      <input type='text' name='content' value={content} onChange={onChange} />
+      <button className='btn btn-dark btn-block' onClick={onSubmit}>
         Add Comment
       </button>
     </div>
