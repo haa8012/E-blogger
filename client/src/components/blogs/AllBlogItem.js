7bb19c26d981eb6dcdfb268adc60e157239e8029
Hani A <ha8012@yahoo.com>
2020-06-08 22:37:31 -0400

    added blog rich editor

--- client/src/components/blogs/AllBlogItem.js
+++ client/src/components/blogs/AllBlogItem.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import dateFormat from 'dateformat';
 import BlogContext from '../../context/blog/blogContext';
 import UserContext from '../../context/auth/authContext';
+import parse from 'html-react-parser';
 
 import { Link } from 'react-router-dom';
 
@@ -13,14 +14,15 @@ const AllBlogItem = ({ blog }) => {
   const blogContext = useContext(BlogContext);
   const { setCurrent } = blogContext;
 
-  const { _id, title, date, image, detail, footer, likes } = blog;
+  const { _id, title, date, image, detail, footer, likes, blogContent } = blog;
   return (
     <div className='card'>
       <h3 className='text-primary text-left p-nb'>{title} </h3>
-      {user && <img className='avatar left-align ' src={user.avatar} alt='' />}
+      {/* {user && <img className='avatar left-align ' src={user.avatar} alt='' />} */}
       <div className='text-left p-nt'>{dateFormat(date, 'mmmm, dd, yyyy')}</div>
       {image && <img src={image} alt='' />}
       <ul className='list'>
+        {blogContent && parse(blogContent)}
         {detail && <p className='text-left px-1 detail'>{detail}</p>}
         {footer && <li className='text-left px-1'>{footer}</li>}
       </ul>
